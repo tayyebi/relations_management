7041da9c9a13e526791db5211a93d77133976231
tayyebi <smile@tyyi.net>
2023-09-02 12:28:44 +0330

    contianer is now full screen.

--- app/view/Report/Index.php
+++ app/view/Report/Index.php
@@ -9,7 +9,7 @@ $row = isset($Data['Row']) ? $Data['Row'] : null;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Report</title>
 </head>
-<body>
+<body style="height: 100%; width: 100%; padding: 0; margin: 0;">
 	<embed width="100%" height="100%" src="<?php echo $row['PROJECT_LINK'] ?>" type="">
 </body>
 </html>

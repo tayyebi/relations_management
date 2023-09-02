97607b30e256d6d90a8fcc47ae7f5819a4cf69be
tayyebi <smile@tyyi.net>
2023-09-02 12:30:18 +0330

    style in header

--- app/view/Report/Index.php
+++ app/view/Report/Index.php
@@ -4,12 +4,25 @@ $row = isset($Data['Row']) ? $Data['Row'] : null;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Report</title>
+	<style>
+		html,
+		body,
+		embed {
+			height: 100%;
+			width: 100%;
+			padding: 0;
+			margin: 0;
+		}
+	</style>
 </head>
-<body style="height: 100%; width: 100%; padding: 0; margin: 0;">
-	<embed width="100%" height="100%" src="<?php echo $row['PROJECT_LINK'] ?>" type="">
+
+<body>
+	<embed src="<?php echo $row['PROJECT_LINK'] ?>" type="">
 </body>
+
 </html>

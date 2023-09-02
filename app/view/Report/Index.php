9b570d48138707804aec800606b08d4989722a38
tayyebi <smile@tyyi.net>
2023-09-02 12:22:29 +0330

    iframe to embed.

--- app/view/Report/Index.php
+++ app/view/Report/Index.php
@@ -2,4 +2,14 @@
 $row = isset($Data['Row']) ? $Data['Row'] : null;
 ?>
 
-<iframe src="<?php echo $row['PROJECT_LINK'] ?>" frameborder="0"></iframe>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Report</title>
+</head>
+<body>
+	<embed src="<?php echo $row['PROJECT_LINK'] ?>" type="">
+</body>
+</html>
